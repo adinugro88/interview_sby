e053fcf8ba61a1d49e3c6c51210367e2fa616a3e
adinugro88 <adin5337@gmail.com>
2024-02-28 14:56:43 +0700

    migration

---
+++ praktek_web/app/Models/Master_invetory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Master_invetory extends Model
+{
+    use HasFactory;
+}
---
+++ praktek_web/database/migrations/2024_02_28_074624_create_transaksi_belis_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksi_belis', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('barang_id');
+            $table->integer('jumlah_item');
+            $table->timestamps();
+            $table->foreign('barang_id')->references('fieldinventory_id')->on('master_invetories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksi_belis');
+    }
+};
